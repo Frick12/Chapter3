87867180fbeedef088bb5c416e89e5d83b527e26
Frick12 <harpodouglas@gmail.com>
2017-02-03 14:40:39 -0600

    Update

--- src/youDoIt/ParadiseInfo2.java
+++ src/youDoIt/ParadiseInfo2.java
@@ -37,8 +37,6 @@ public class ParadiseInfo2
 	
 	public static void displayInfo()
 	{
-
-		
 		System.out.println("Paradise Day Spa wants to pamper you.");
 		System.out.println("We will make you look good.");
 	}
