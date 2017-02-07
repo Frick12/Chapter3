7fc0bc14b3ca276357b884434e9a5334fea9ddf5
Frick12 <harpodouglas@gmail.com>
2017-02-07 15:00:06 -0600

    Update

--- src/exercisesPage175/fiveB.java
+++ src/exercisesPage175/fiveB.java
@@ -5,7 +5,7 @@ public class fiveB
 
 	public static void main(String[] args) 
 	{
-		double 
+		
 	}
 
 }
--- src/gameZonePage178/Card.java
+++ src/gameZonePage178/Card.java
@@ -1,10 +1,23 @@
 package gameZonePage178;
 
-public class Card {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class Card
+{
+   private char suit;
+   private int value;
+   public char getSuit()
+   {
+      return suit;
+   }
+   public int getValue()
+   {
+      return value;
+   }
+   public void setSuit(char s)
+   {
+      suit = s;
+   }
+   public void setValue(int v)
+   {
+      value = v;
+   }
 }
