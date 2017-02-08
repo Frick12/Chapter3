9c03487a57a2c3fda06eaa0d3624a6491a5e26dd
Frick12 <harpodouglas@gmail.com>
2017-02-08 13:18:27 -0600

    Update

--- src/exercisesPage175/fiveB.java
+++ src/exercisesPage175/fiveB.java
@@ -5,7 +5,19 @@ public class fiveB
 
 	public static void main(String[] args) 
 	{
+		double numOne = 2;
+		double numTwo = 5;
+		double percentage;
 		
+		percentage = computePercent(numOne, numTwo);
+		
+		System.out.println("The percentage is " + percentage + " percent.");
 	}
-
+	
+	public static double computePercent(double numOne, double numTwo)
+	{
+		double percentage;
+		percentage = (numOne / numTwo) * 100 ;
+		return percentage;
+	}
 }
--- src/gameZonePage178/PickTwoCards.java
+++ src/gameZonePage178/PickTwoCards.java
@@ -9,12 +9,14 @@ public class PickTwoCards
       int yourValue;
       Card myCard = new Card();
       Card yourCard = new Card();
+     
       myValue = ((int)(Math.random() * 100) % CARDS_IN_SUIT + 1);
       yourValue = ((int)(Math.random() * 100) % CARDS_IN_SUIT + 1);
       myCard.setValue(myValue);
       yourCard.setValue(yourValue);
       myCard.setSuit('s');
       yourCard.setSuit('h');
+      
       System.out.println("My card is the " + myCard.getValue() + " of " + myCard.getSuit());
       System.out.println("Your card is the " + yourCard.getValue() + " of " + yourCard.getSuit());
    }
